d30f77a3c4d6f779b2ffd8983b4aa7ddb571f626
Rahul Kumar <rahul.k@panorbit.in>
2020-10-29 16:01:50 +0530

    Chat setup

--- chat/views.py
+++ chat/views.py
@@ -7,5 +7,4 @@ def index(request):
 
 def room(request, room_name):
     return render(request, 'chat/room.html', {
-        'room_name': room_name
-    })
+        'room_name': room_name})
--- djangoChannels/settings.py
+++ djangoChannels/settings.py
@@ -120,3 +120,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+ASGI_APPLICATION = 'djangoChannels.routing.application'
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
